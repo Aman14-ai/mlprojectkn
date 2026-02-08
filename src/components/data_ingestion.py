0ec3914e9f8d1859f89103c73227b0315b5836ae
Aman14-ai <amancode2005@gmail.com>
2026-02-08 15:52:43 +0530

    model trainer

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -2,10 +2,11 @@ import os
 import sys
 from src.logger import logging
 from src.exception import CustomException
-from src.components.data_transformation import DataTransformation,DataTransformationConfig
+from src.components.data_transformation import DataTransformation
 from dataclasses import dataclass
 import pandas as pd
 from sklearn.model_selection import train_test_split
+from src.components.models_trainer import ModelTrainer
 
 @dataclass
 class DataIngestionConfig:
@@ -44,3 +45,8 @@ if __name__=="__main__":
 
     data_transformation=DataTransformation()
     train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data_path,test_data_path)
+
+    model_trainer=ModelTrainer()
+    r2_score=model_trainer.initiate_model_trainer(train_arr,test_arr)
+    print(f"R2 Score : {r2_score}")
+    
